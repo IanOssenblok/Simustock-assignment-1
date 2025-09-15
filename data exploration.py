b097dd61a5dd8f574ac5f68d2966512deb4fa9f3
IanOssenblok <ian@ianossenblok.com>
2025-09-15 10:26:57 +0200

    basic exploration

--- data exploration.py
+++ data exploration.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-data = pd.read_excel("data/data_assignment.xlsx")
-print(data.head())
-
-
+df = pd.read_excel("data/data_assignment.xlsx")
+# print(df.head())
+# print(df.columns) # Index(['Date', 'Category', 'Income/Expense', 'Amount'], dtype='object')
+print(df["Amount"].describe())
+# print(df.isna().sum()) # no missing values
 
